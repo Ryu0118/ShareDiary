b488459bcf256d978fb2872098a6171b199b1abb
Ryu0118 <ryunosuke.shibuya.3d@stu.hosei.ac.jp>
2022-04-29 16:52:54 +0900

    first commit

---
+++ ShareDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShareDiary
+//
+//  Created by Ryu on 2022/04/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
